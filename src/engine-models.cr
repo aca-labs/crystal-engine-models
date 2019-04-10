3cd5b35760b18f96b2528cd9b876dd161a31e61c
Caspian Baska <caspianbaska@gmail.com>
2019-04-10 15:27:18 +1000

    (engine-models) add SubModel abstract class for validations for params and embeded documents

--- src/engine-models.cr
+++ src/engine-models.cr
@@ -1,9 +1,19 @@
 require "rethinkdb-orm"
 require "neuroplastic"
+require "active-model"
 
 module Engine::Model
   abstract class ModelBase < RethinkORM::Base
     include Neuroplastic
+  end
+
+  abstract class SubModel < ActiveModel::Model
+    include ActiveModel::Validation
+
+    # Generate string for field
+    def to_reql
+      self.to_json
+    end
   end
 end
 
