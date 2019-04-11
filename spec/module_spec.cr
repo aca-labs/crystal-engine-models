d120a3816cd722410f5c570c121dc92321534faa
Caspian Baska <caspianbaska@gmail.com>
2019-04-11 16:04:52 +1000

    (module) refactor module validation

--- spec/module_spec.cr
+++ spec/module_spec.cr
@@ -1,4 +1,101 @@
 require "./helper"
 
 describe Engine::Model::Module do
+  describe "persistence" do
+    it "saves a Service module" do
+      name = Faker::Hacker.noun
+      mod = Engine::Model::Module.new(
+        custom_name: name,
+        uri: Faker::Internet.url,
+      )
+
+      service_dep = new_dependency(
+        module_name: name,
+        role: Engine::Model::Dependency::Role::Service,
+      ).save!
+      mod.dependency = service_dep
+
+      begin
+        mod.save!
+        mod.persisted?.should be_true
+      rescue e : RethinkORM::Error::DocumentInvalid
+        inspect_error(e)
+        raise e
+      end
+    end
+
+    it "saves a Logic module" do
+      name = Faker::Hacker.noun
+      mod = Engine::Model::Module.new(
+        custom_name: name,
+        uri: Faker::Internet.url,
+      )
+
+      service_dep = new_dependency(
+        module_name: name,
+        role: Engine::Model::Dependency::Role::Logic,
+      ).save!
+
+      mod.dependency = service_dep
+      mod.control_system = new_control_system.save!
+
+      begin
+        mod.save!
+        mod.persisted?.should be_true
+      rescue e : RethinkORM::Error::DocumentInvalid
+        inspect_error(e)
+        raise e
+      end
+    end
+
+    it "saves a Device module" do
+      name = Faker::Hacker.noun
+      mod = Engine::Model::Module.new(
+        custom_name: name,
+        uri: Faker::Internet.url,
+        ip: Faker::Internet.ip_v4_address,
+        port: Random.rand((1..6555)),
+      )
+
+      service_dep = new_dependency(
+        module_name: name,
+        role: Engine::Model::Dependency::Role::Device,
+      ).save!
+
+      mod.dependency = service_dep
+
+      begin
+        mod.save!
+        mod.persisted?.should be_true
+      rescue e : RethinkORM::Error::DocumentInvalid
+        inspect_error(e)
+        raise e
+      end
+    end
+
+    it "saves a SSH module" do
+      name = Faker::Hacker.noun
+      mod = Engine::Model::Module.new(
+        custom_name: name,
+        uri: Faker::Internet.url,
+        ip: Faker::Internet.ip_v4_address,
+        port: Random.rand((1..6555)),
+      )
+
+      service_dep = new_dependency(
+        module_name: name,
+        role: Engine::Model::Dependency::Role::SSH,
+      ).save!
+
+      mod.dependency = service_dep
+
+      begin
+        mod.save!
+        mod.persisted?.should be_true
+      rescue e : RethinkORM::Error::DocumentInvalid
+        inspect_error(e)
+        raise e
+      end
+    end
+  end
 end
--- src/engine-models/module.cr
+++ src/engine-models/module.cr
@@ -8,6 +8,7 @@ module Engine::Model
 
     # The classes / files that this module requires to execute
     # Defines module type
+
     # Requires dependency_id to be set
     belongs_to Dependency
     belongs_to ControlSystem
@@ -25,8 +26,8 @@ module Engine::Model
     attribute custom_name : String
     attribute settings : String = "{}"
 
-    attribute updated_at : Time = ->{ Time.now }
-    attribute created_at : Time = ->{ Time.now }
+    attribute updated_at : Time = ->{ Time.utc_now }, converter: Time::EpochConverter
+    attribute created_at : Time = ->{ Time.utc_now }, converter: Time::EpochConverter
 
     enum_attribute role : Dependency::Role # cache the dependency role locally for load order
 
@@ -65,51 +66,68 @@ module Engine::Model
     validate ->(this : Module) {
       dependency = this.dependency
       return if dependency.nil?
-
       case dependency.role
       when Dependency::Role::Service
-        this.role = Dependency::Role::Service
-        this.udp = false
-
-        this.uri ||= dependency.default_uri
-
-        uri = this.uri # URI presence
-        unless uri
-          validation_error(:uri, "not present")
-          return
-        end
-
-        url = URI.parse(uri)
-        url_parsed = !!(url.host && url.scheme)     # Ensure URL can be parsed
-        this.tls = !!(url && url.scheme == "https") # Secure indication
-
-        validation_error(:uri, "is an invalid URI") unless url_parsed
+        this.validate_service_module
       when Dependency::Role::Logic
-        this.connected = true # Logic modules are connectionless
-        this.tls = nil
-        this.udp = nil
-        this.role = Dependency::Role::Logic
-        has_control = !this.control_system.nil?
-
-        validation_error(:control_system, "must be associated") unless has_control
+        this.validate_logic_module
       when Dependency::Role::Device, Dependency::Role::SSH
-        this.role = dependency.role
-        this.port = (this.port || dependency.default_port || 0).to_i
-        ip = this.ip
-        port = this.port
-
-        # No blank IP
-        validation_error(:ip, "cannot be blank") if ip && ip.blank?
-        # Port in valid range
-        validation_error(:port, "is invalid") unless port && (1..65535) === port
-
-        this.tls = false if this.udp
-
-        url = URI.parse("http://#{ip}:#{port}/")
-        url_parsed = !!(url.scheme && url.host)
-
-        validation_error(:ip, "address / hostname or port are not valid") unless url_parsed
+        this.validate_device_module
       end
     }
+
+    protected def validate_service_module
+      self.role = Dependency::Role::Service
+      self.udp = false
+
+      dependency = self.dependency
+      return if dependency.nil?
+
+      self.uri ||= dependency.default_uri
+
+      uri = self.uri # URI presence
+      unless uri
+        self.validation_error(:uri, "not present")
+        return
+      end
+
+      url = URI.parse(uri)
+      url_parsed = !!(url.host && url.scheme)     # Ensure URL can be parsed
+      self.tls = !!(url && url.scheme == "https") # Secure indication
+
+      self.validation_error(:uri, "is an invalid URI") unless url_parsed
+    end
+
+    protected def validate_logic_module
+      self.connected = true # Logic modules are connectionless
+      self.tls = nil
+      self.udp = nil
+      self.role = Dependency::Role::Logic
+      has_control = !self.control_system_id.nil?
+
+      self.validation_error(:control_system, "must be associated") unless has_control
+    end
+
+    protected def validate_device_module
+      dependency = self.dependency
+      return if dependency.nil?
+
+      self.role = dependency.role
+      self.port = (self.port || dependency.default_port || 0).to_i
+      ip = self.ip
+      port = self.port
+
+      # No blank IP
+      self.validation_error(:ip, "cannot be blank") if ip && ip.blank?
+      # Port in valid range
+      self.validation_error(:port, "is invalid") unless port && (1..65535) === port
+
+      self.tls = false if self.udp
+
+      url = URI.parse("http://#{ip}:#{port}/")
+      url_parsed = !!(url.scheme && url.host)
+
+      self.validation_error(:ip, "address / hostname or port are not valid") unless url_parsed
+    end
   end
 end
