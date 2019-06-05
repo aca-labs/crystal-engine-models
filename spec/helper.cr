2d7df9244b43b65a1942e148f26019865d20a234
Caspian Baska <caspianbaska@gmail.com>
2019-06-05 16:14:04 +1000

    (model) expose the rethinkdb connection beneath model namespace

--- spec/helper.cr
+++ spec/helper.cr
@@ -5,13 +5,13 @@ require "rethinkdb-orm"
 require "./generator"
 
 DB_NAME = "test_#{Time.now.to_unix}_#{rand(10000)}"
-RethinkORM::Connection.configure do |settings|
+Engine::Model::Connection.configure do |settings|
   settings.db = DB_NAME
 end
 
 # Tear down the test database
 at_exit do
-  RethinkORM::Connection.raw do |q|
+  Engine::Model::Connection.raw do |q|
     q.db_drop(DB_NAME)
   end
 end
--- src/engine-models.cr
+++ src/engine-models.cr
@@ -30,6 +30,11 @@ module Engine::Model
       end
     end
   end
+
+  # Expose RethinkDB connection
+  # Use for configuration, raw queries
+  class Connection < RethinkORM::Connection
+  end
 end
 
 require "./engine-models/*"
