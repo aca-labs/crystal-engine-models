f4e56fa27951e7d09928cc26fbb1e41712fa3d25
Stephen von Takach <steve@acaprojects.com>
2020-04-20 09:23:08 +1000

    feat(user_auth_lookup): clean up on user destroy

---
+++ src/models/user_auth_lookup.cr
@@ -0,0 +1,21 @@
+require "rethinkdb-orm"
+require "./base/model"
+require "./authority"
+require "./user"
+
+module PlaceOS::Model
+  class UserAuthLookup < ModelBase
+    include RethinkORM::Timestamps
+    table :authentication
+
+    attribute uid : String
+    attribute provider : String
+    belongs_to User
+    belongs_to Authority
+
+    before_create :generate_id
+    def generate_id
+      self.id = "auth-#{self.authority_id}-#{self.provider}-#{self.uid}"
+    end
+  end
+end
