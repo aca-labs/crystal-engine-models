3ecde8268e9df2265890287b1093249da1020e2a
Stephen von Takach <steve@acaprojects.com>
2020-04-22 13:22:17 +1000

    fix(statistics spec): ensure ttl isn't nil

--- spec/statistics_spec.cr
+++ spec/statistics_spec.cr
@@ -18,7 +18,7 @@ module PlaceOS::Model
       id = stats.id
       id.should start_with "stats-" if id
 
-      ttl = stats.ttl
+      ttl = stats.ttl.not_nil!
       ttl.should be < 31.days.from_now.to_unix
       ttl.should be > 29.days.from_now.to_unix
 
