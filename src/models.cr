8acfe97f3b2347d217323011d53cb494b9567daa
Caspian Baska <caspianbaska@gmail.com>
2020-04-27 16:00:25 +1000

    fix(user): catch auth cleanup failures

--- src/models.cr
+++ src/models.cr
@@ -1,8 +1,11 @@
+require "log"
 require "rethinkdb-orm"
 
 require "./models/base/*"
 
 module PlaceOS::Model
+  Log = ::Log.for(self)
+
   # Expose RethinkDB connection
   # Use for configuration, raw queries
   class Connection < RethinkORM::Connection
