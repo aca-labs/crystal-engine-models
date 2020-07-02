f468ac6f3936a747cdbb259903c64d0a1dcdc619
Caspian Baska <caspianbaska@gmail.com>
2020-07-02 16:03:22 +1000

    fix(metadata): rename `Response` to `Interface`; set non-nillable fields

--- spec/metadata_spec.cr
+++ spec/metadata_spec.cr
@@ -3,13 +3,8 @@ require "./helper"
 module PlaceOS::Model
   describe Metadata do
     it "saves control_system metadata" do
-      control_system = Generator.control_system
-      control_system.save!
-
-      meta = Metadata.new
-      meta.parent_id = control_system.id
-      meta.name = "test"
-      meta.save!
+      control_system = Generator.control_system.save!
+      meta = Generator.metadata(name: "test", parent: control_system.id.as(String)).save!
 
       control_system.metadata.first.id.should eq meta.id
       meta_find = Metadata.find!(meta.id.as(String))
@@ -17,13 +12,8 @@ module PlaceOS::Model
     end
 
     it "saves zone metadata" do
-      zone = Generator.zone
-      zone.save!
-
-      meta = Metadata.new
-      meta.parent_id = zone.id
-      meta.name = "test"
-      meta.save!
+      zone = Generator.zone.save!
+      meta = Generator.metadata(name: "test", parent: zone.id.as(String)).save!
 
       zone.metadata.first.id.should eq meta.id
       meta_find = Metadata.find!(meta.id.as(String))
