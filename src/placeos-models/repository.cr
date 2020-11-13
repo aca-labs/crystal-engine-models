22d92063e1b8c284676c5411c84419b990e69917
Caspian Baska <caspianbaska@gmail.com>
2020-11-13 11:47:34 +1100

    fix(repository): validate no spaces in `folder_name`

--- src/placeos-models/repository.cr
+++ src/placeos-models/repository.cr
@@ -22,8 +22,11 @@ module PlaceOS::Model
 
     # Repository metadata
     attribute name : String, es_subfield: "keyword"
+    attribute description : String = ""
+
+    # folder_name may only contain valid path characters
     attribute folder_name : String
-    attribute description : String
+
     attribute uri : String
     attribute commit_hash : String = "HEAD"
     attribute branch : String = "master"
@@ -32,7 +35,7 @@ module PlaceOS::Model
 
     # Validations
     validates :name, presence: true
-    validates :folder_name, presence: true
+    validates :folder_name, presence: true, format: {with: /^[a-zA-Z0-9_+\-\(\)\.]*$/}
     validates :repo_type, presence: true
     validates :uri, presence: true
     validates :commit_hash, presence: true
