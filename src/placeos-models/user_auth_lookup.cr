24261c95c16cb8847076b719781e414be8be50ac
Caspian Baska <caspianbaska@gmail.com>
2021-04-23 18:29:12 +1000

    fix(user_auth_lookup): set new_flag in generate_id callback

--- src/placeos-models/user_auth_lookup.cr
+++ src/placeos-models/user_auth_lookup.cr
@@ -6,16 +6,25 @@ require "./user"
 module PlaceOS::Model
   class UserAuthLookup < ModelBase
     include RethinkORM::Timestamps
+
     table :authentication
 
     attribute uid : String
     attribute provider : String
+
+    # Association
+    ###############################################################################################
+
     belongs_to User
     belongs_to Authority
 
+    # Callbacks
+    ###############################################################################################
+
     before_create :generate_id
 
-    def generate_id
+    protected def generate_id
+      self._new_flag = true
       self.id = "auth-#{self.authority_id}-#{self.provider}-#{self.uid}"
     end
   end
