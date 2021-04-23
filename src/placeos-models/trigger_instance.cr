990b2c2ffd0b27fc5919211b1dc834f784c9b808
Caspian Baska <caspianbaska@gmail.com>
2021-04-23 19:21:06 +1000

    style(trigger_instance): improve class layout

--- src/placeos-models/trigger_instance.cr
+++ src/placeos-models/trigger_instance.cr
@@ -7,76 +7,60 @@ require "./base/model"
 module PlaceOS::Model
   class TriggerInstance < ModelBase
     include RethinkORM::Timestamps
-    table :trig
 
-    belongs_to ControlSystem, foreign_key: "control_system_id"
-    belongs_to Trigger, foreign_key: "trigger_id"
-    belongs_to Zone, foreign_key: "zone_id"
+    table :trig
 
     attribute enabled : Bool = true
     attribute triggered : Bool = false
     attribute important : Bool = false
     attribute exec_enabled : Bool = false
 
-    attribute webhook_secret : String = ->{ Random::Secure.hex(32) }
+    attribute webhook_secret : String = ->{ Random::Secure.urlsafe_base64(32) }
     attribute trigger_count : Int32 = 0
+
+    # Association
+    ################################################################################################
+
+    belongs_to ControlSystem, foreign_key: "control_system_id"
+    belongs_to Trigger, foreign_key: "trigger_id"
+    belongs_to Zone, foreign_key: "zone_id"
+
+    # Validation
+    ################################################################################################
+
+    validates :control_system, presence: true
+    validates :trigger, presence: true
+
+    # Callbacks
+    ################################################################################################
 
     before_create :set_importance
 
-    # Increment the trigger_count of a TriggerInstance in place
-    #
-    def self.increment_trigger_count(id : String)
-      TriggerInstance.table_query do |q|
-        q.get(id).update do |doc|
-          doc.merge({"trigger_count" => doc["trigger_count"].add(1)})
-        end
+    protected def set_importance
+      if (trig = self.trigger)
+        self.important = trig.important
       end
     end
 
-    # ----------------
-    # PARENT ACCESSORS
-    # ----------------
+    # Queries
+    ################################################################################################
 
-    def name
-      self.trigger.try(&.name)
-    end
-
-    def description
-      self.trigger.try(&.description)
-    end
-
-    def conditions
-      self.trigger.try(&.conditions)
-    end
-
-    def actions
-      self.trigger.try(&.actions)
-    end
-
-    def debounce_period
-      self.trigger.try(&.debounce_period)
-    end
-
-    def binding
-      self.id
-    end
-
-    # ------------
-    # VIEWS ACCESS
-    # ------------
-
-    # Look up TriggerInstances by ControlSystem
+    # Look up `TriggerInstance`s by `ControlSystem`
     def self.for(control_system_id)
       TriggerInstance.by_control_system_id(control_system_id)
     end
 
-    # Look up TriggerInstances belonging to Trigger
+    # Look up `TriggerInstance`s belonging to `Trigger`
     def self.of(trigger_id)
       TriggerInstance.by_trigger_id(trigger_id)
     end
 
+    # Serialisation
+    ################################################################################################
+
     # Override to_json, set method fields
     def as_json
+      # NOTE: This pattern is fine as no attributes have special serialisation.
       self.attributes.merge({
         :name        => name,
         :description => description,
@@ -86,20 +70,17 @@ module PlaceOS::Model
       }).to_json
     end
 
-    # --------------------
-    # START / STOP HELPERS
-    # --------------------
-
-    protected def set_importance
-      self.important = self.trigger.try &.important || self.important
-    end
+    # TriggerInstance State
+    ################################################################################################
 
     # Enables the trigger
+    #
     def start
       toggle_trigger(true)
     end
 
     # Disables the trigger
+    #
     def stop
       toggle_trigger(false)
     end
@@ -108,12 +89,31 @@ module PlaceOS::Model
       self.update_fields(enabled: on)
     end
 
-    # -----------
-    # VALIDATIONS
-    # -----------
+    # Increment the `trigger_count` of a `TriggerInstance` in place.
+    #
+    def self.increment_trigger_count(id : String)
+      TriggerInstance.table_query do |q|
+        q.get(id).update do |doc|
+          doc.merge({"trigger_count" => doc["trigger_count"].add(1)})
+        end
+      end
+    end
 
-    # Ensure the models exist in the database
-    validates :control_system, presence: true
-    validates :trigger, presence: true
+    # Proxied `Trigger` attributes
+    ################################################################################################
+
+    {% begin %}
+      {% for attr in {:name, :description, :conditions, :actions, :debounce_period} %}
+        # Proxies the parent `Trigger`'s {{attr.id}} attribute.
+        def {{ attr.id }}
+          trigger.try &.{{ attr.id }}
+        end
+      {% end %}
+    {% end %}
+
+    @[Deprecated("Use `#id` instead.")]
+    def binding
+      self.id
+    end
   end
 end
