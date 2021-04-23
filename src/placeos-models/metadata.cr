2c9bb837806ba5cf2fa76498daaca2a68d0b8a22
Caspian Baska <caspianbaska@gmail.com>
2021-04-23 17:18:51 +1000

    style(metadata): improve class layout

--- src/placeos-models/metadata.cr
+++ src/placeos-models/metadata.cr
@@ -22,11 +22,17 @@ module PlaceOS::Model
 
     attribute parent_id : String, es_type: "keyword"
 
+    # Association
+    ###############################################################################################
+
     secondary_index :parent_id
 
     belongs_to Zone, foreign_key: "parent_id", association_name: "zone"
     belongs_to ControlSystem, foreign_key: "parent_id", association_name: "control_system"
     belongs_to User, foreign_key: "parent_id", association_name: "user"
+
+    # Validation
+    ###############################################################################################
 
     validates :details, presence: true
     validates :name, presence: true
@@ -35,6 +41,30 @@ module PlaceOS::Model
     ensure_unique :name, scope: [:parent_id, :name] do |parent_id, name|
       {parent_id, name.strip.downcase}
     end
+
+    # Queries
+    ###############################################################################################
+
+    def self.for(parent : String | Zone | ControlSystem | User, name : String? = nil)
+      parent_id = case parent
+                  in String
+                    parent
+                  in Zone, ControlSystem, User
+                    parent.id.as(String)
+                  end
+
+      Metadata.raw_query do |q|
+        query = q.table(Model::Metadata.table_name).get_all(parent_id, index: :parent_id)
+        if name && !name.empty?
+          query.filter({name: name})
+        else
+          query
+        end
+      end
+    end
+
+    # Serialisation
+    ###############################################################################################
 
     record Interface, name : String, description : String, details : JSON::Any, editors : Set(String)?, parent_id : String? {
       include JSON::Serializable
@@ -53,24 +83,6 @@ module PlaceOS::Model
     def self.build_metadata(parent, name : String? = nil) : Hash(String, Interface)
       for(parent, name).each_with_object({} of String => Interface) do |data, results|
         results[data.name] = self.interface(data)
-      end
-    end
-
-    def self.for(parent : String | Zone | ControlSystem | User, name : String? = nil)
-      parent_id = case parent
-                  in String
-                    parent
-                  in Zone, ControlSystem, User
-                    parent.id.as(String)
-                  end
-
-      Metadata.raw_query do |q|
-        query = q.table(Model::Metadata.table_name).get_all(parent_id, index: :parent_id)
-        if name && !name.empty?
-          query.filter({name: name})
-        else
-          query
-        end
       end
     end
   end
