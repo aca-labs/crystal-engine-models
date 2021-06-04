fbd849a00019fa2f6baac3463bd5124541903a6f
Caspian Baska <email@caspian.computer>
2021-06-04 17:56:56 +1000

    fix(version): use string for date field

--- src/placeos-models/version.cr
+++ src/placeos-models/version.cr
@@ -1,5 +1,5 @@
 require "json"
 
-record PlaceOS::Model::Version, service : String, commit : String, build_time : Int64, version : String do
+record PlaceOS::Model::Version, service : String, commit : String, build_time : String, version : String do
   include JSON::Serializable
 end
